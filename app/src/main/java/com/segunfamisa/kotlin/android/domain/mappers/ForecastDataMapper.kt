295517df96336bd98cb1506459ee070de803fc67
Segun Famisa <segunfamisa@gmail.com>
2017-01-03 20:08:57 +0100

    [chore] modify domain class and mapper to accomodate weather icon

--- app/src/main/java/com/segunfamisa/kotlin/android/domain/mappers/ForecastDataMapper.kt
+++ app/src/main/java/com/segunfamisa/kotlin/android/domain/mappers/ForecastDataMapper.kt
@@ -26,11 +26,16 @@ class ForecastDataMapper {
     private fun convertForecastItemToDomain(forecast: ModelForecast):
             Forecast {
         return Forecast(convertDate(forecast.dt), forecast.weather[0].description,
-                forecast.temp.max.toInt(), forecast.temp.min.toInt())
+                forecast.temp.max.toInt(), forecast.temp.min.toInt(),
+                generateIconUrl(forecast.weather[0].icon))
     }
 
     private fun convertDate(date: Long): String {
         val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
         return df.format(date * 1000)
+    }
+
+    private fun generateIconUrl(iconCode: String): String {
+        return "http://openweathermap.org/img/w/$iconCode.png"
     }
 }
