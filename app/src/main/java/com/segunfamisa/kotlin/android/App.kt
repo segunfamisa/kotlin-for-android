60e3cf8709cad933e8b2f8749671336796c4cdca
Segun Famisa <segunfamisa@gmail.com>
2017-01-17 20:19:46 +0100

    [chore] add app context singleton

---
+++ app/src/main/java/com/segunfamisa/kotlin/android/App.kt
@@ -0,0 +1,19 @@
+package com.segunfamisa.kotlin.android
+
+import android.app.Application
+
+
+class App : Application() {
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+    }
+}
+
